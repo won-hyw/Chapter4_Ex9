9a288d10be86e9a63cfc85ad6bcca0ddb395c9f1
choi ji won <w2019@e-mirim.hs.kr>
2021-05-04 20:10:02 +0900

    finish commit

--- app/src/main/java/kr/hs/emirim/w2019/chapter4_ex9/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/chapter4_ex9/MainActivity.java
@@ -3,12 +3,28 @@ package kr.hs.emirim.w2019.chapter4_ex9;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imgv;
+    int angle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnRotate = findViewById(R.id.btn_rotate);
+        imgv = findViewById(R.id.imgv);
+        btnRotate.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            angle += 10;
+            imgv.setRotation(angle);
+        }
+    };
 }
